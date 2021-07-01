80928dca12acd5e57d19b8849aa7f47127dda4a6
julieconnors <juliana.g.connors@gmail.com>
2021-07-01 15:59:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function findMinAndRemoveSorted(a) {
+    return a.shift()
+}
 
+function merge(a1, a2) {
+    let sorted = []
+    while(a1.length !== 0 && a2.length !== 0) {
+        if(a1[0] < a2[0]) {
+            sorted.push(findMinAndRemoveSorted(a1))
+        } else {
+            sorted.push(findMinAndRemoveSorted(a2))
+        }
+        
+    }
+    return sorted.concat(a1).concat(a2)
+}
+
+function mergeSort(a) {
+    let mid = a.length / 2
+    let first = a.slice(0, mid)
+    let last = a.slice(mid, a.length)
+    let sorted;
+
+    if (a.length < 2) {
+        return a
+    } else {
+        sorted = merge(mergeSort(first), mergeSort(last))
+    }
+    return sorted
+}
